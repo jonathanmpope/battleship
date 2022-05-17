cd0ec8b5dce535c5183b565804b725ff170a658e
Brenna Stuart <brennacodes@gmail.com>
2022-05-17 15:38:51 -0600

    Feat: pass valid_placement

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -35,6 +35,7 @@ RSpec.describe do
       expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq true
       expect(board.valid_placement?(submarine, ["A1", "A2", "A3"])).to eq false
       expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq true
+      expect(board.valid_placement?(submarine, ["A1", "B2"])).to eq false
       expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to eq true
     end
 end
